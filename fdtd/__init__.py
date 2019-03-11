bbae0223bdd05a187d204c107eecf1613653fbfa
flaport <floris.laporte@gmail.com>
2019-03-11 14:18:15 +0100

    added a setup file

--- fdtd/__init__.py
+++ fdtd/__init__.py
@@ -1,5 +1,8 @@
 """ Python 3D FDTD Simulator """
 
+__author__ = "Floris laporte"
+__version__ = "0.0.0"
+
 from .grid import Grid
 from .sources import LineSource
 from .detectors import Detector
---
+++ setup.py
@@ -0,0 +1,24 @@
+import fdtd
+import setuptools
+
+description = """a 3D electromagnetic FDTD simulator written in Python"""
+
+setuptools.setup(
+    name=fdtd.__name__,
+    version=fdtd.__version__,
+    author=fdtd.__author__,
+    author_email="floris.laporte@gmail.com",
+    description=description,
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/flaport/fdtd",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+)
