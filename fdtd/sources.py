d204188ccfa4c035df6d8056c24741a39f3131f4
flaport <floris.laporte@gmail.com>
2019-03-11 17:18:13 +0100

    better handling of slices in LineSource

--- fdtd/sources.py
+++ fdtd/sources.py
@@ -20,7 +20,7 @@ from .backend import backend as bd
 
 ## LineSource class
 class LineSource:
-    """ A source along a line in the grid """
+    """ A source along a line in the FDTD grid """
 
     def __init__(
         self,
@@ -37,10 +37,6 @@ class LineSource:
             power = 1.0: The power of the source
             phase_shift = 0.0: The phase offset of the source.
 
-        Note:
-            The initialization of the source is not finished before it is
-            registered on the grid.
-
         """
         self.grid = None
         self.period = period
@@ -54,10 +50,10 @@ class LineSource:
         """ Register a grid for the source.
 
         Args:
-            grid: The grid to register the source in.
-            x: The x-location of the volume in the grid
-            y: The y-location of the volume in the grid
-            z: The z-location of the volume in the grid
+            grid: the grid to place the source into.
+            x: The x-location of the source in the grid
+            y: The y-location of the source in the grid
+            z: The z-location of the source in the grid
 
         Note:
             As its name suggests, this source is a LINE source.
@@ -65,7 +61,7 @@ class LineSource:
             defined by the slices in the grid.
         """
         self.grid = grid
-        self.grid._sources.append(self)
+        self.grid.sources.append(self)
         if self.name is not None:
             if not hasattr(grid, self.name):
                 setattr(grid, self.name, self)
@@ -129,19 +125,24 @@ class LineSource:
         if isinstance(z, list):
             z = slice(z[0], z[-1], None)
 
-        # if we get here, we can assume only well-behaved slices:
-        # we convert the slices into index lists
-        x0, y0, z0 = x.start, y.start, z.start
-        x1, y1, z1 = x.stop, y.stop, z.stop
+        # if we get here, we can assume slices:
+        x0 = x.start if x.start is not None else 0
+        y0 = y.start if y.start is not None else 0
+        z0 = z.start if z.start is not None else 0
+        x1 = x.stop if x.stop is not None else self.grid.Nx
+        y1 = y.stop if y.stop is not None else self.grid.Ny
+        z1 = z.stop if z.stop is not None else self.grid.Nz
+
+        # we can now convert these coordinates into index lists
         m = max(abs(x1 - x0), abs(y1 - y0), abs(z1 - z0))
-        x = list(bd.array(bd.linspace(x0, x1, m, endpoint=False), bd.int))
-        y = list(bd.array(bd.linspace(y0, y1, m, endpoint=False), bd.int))
-        z = list(bd.array(bd.linspace(z0, z1, m, endpoint=False), bd.int))
+        x = [v.item() for v in bd.array(bd.linspace(x0, x1, m, endpoint=False), bd.int)]
+        y = [v.item() for v in bd.array(bd.linspace(y0, y1, m, endpoint=False), bd.int)]
+        z = [v.item() for v in bd.array(bd.linspace(z0, z1, m, endpoint=False), bd.int)]
         return x, y, z
 
     def update_E(self):
         """ Add the source to the electric field """
-        q = self.grid.timesteps_passed
+        q = self.grid.time_steps_passed
         vect = self.profile * sin(2 * pi * q / self.period + self.phase_shift)
         # do not use list indexing here, as this is much slower especially for torch backend
         # DISABLED: self.grid.E[self.x, self.y, self.z, 2] = self.vect
@@ -157,3 +158,11 @@ class LineSource:
             f"power={self.power}, phase_shift={self.phase_shift}, "
             f"name={repr(self.name)})"
         )
+
+    def __str__(self):
+        s = "    " + repr(self) + "\n"
+        x = f"[{self.x[0]}, ... , {self.x[-1]}]"
+        y = f"[{self.y[0]}, ... , {self.y[-1]}]"
+        z = f"[{self.z[0]}, ... , {self.z[-1]}]"
+        s += f"        @ x={x}, y={y}, z={z}\n"
+        return s
