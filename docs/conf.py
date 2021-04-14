d04f4a5bd33d49ed0dd65b96c05e2283f6aaa3ee
flaport <floris.laporte@gmail.com>
2021-04-14 15:03:42 +0200

    add examples to docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,33 +12,43 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+import shutil
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+import fdtd
 
 # -- Project information -----------------------------------------------------
 
-project = 'fdtd'
-copyright = '2020, flaport'
-author = 'flaport'
+project = fdtd.__name__
+copyright = "2021, {fdtd.__author__}"
+author = fdtd.__author__
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = fdtd.__version__
 
 
 # -- General configuration ---------------------------------------------------
 
-master_doc = 'index'
+master_doc = "index"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "nbsphinx",
+    "sphinx_rtd_theme",
+]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -46,12 +56,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+
+# Change how type hints are displayed (requires sphinx.ext.autodoc.typehints)
+autodoc_typehints = "signature"  # signature, description, none
+autodoc_type_aliases = {}
 
 # -- Autodoc ----------------------------------------------------------------
 def skip(app, what, name, obj, would_skip, options):
@@ -59,7 +73,22 @@ def skip(app, what, name, obj, would_skip, options):
         return False
     return would_skip
 
+
 def setup(app):
     app.connect("autodoc-skip-member", skip)
 
+
 autodoc_mock_imports = ["tqdm", "numpy", "matplotlib", "torch"]
+
+
+# -- Examples Folder ---------------------------------------------------------
+
+sourcedir = os.path.dirname(__file__)
+staticdir = os.path.join(sourcedir, "_static")
+fdtd_src = os.path.abspath(os.path.join(sourcedir, "..", "fdtd"))
+examples_src = os.path.abspath(os.path.join(sourcedir, "..", "examples"))
+examples_dst = os.path.abspath(os.path.join(sourcedir, "examples"))
+os.makedirs(staticdir, exist_ok=True)
+shutil.rmtree(examples_dst, ignore_errors=True)
+shutil.copytree(examples_src, examples_dst)
+shutil.copytree(fdtd_src, os.path.join(examples_dst, "fdtd"))
---
+++ docs/examples.rst
@@ -0,0 +1,13 @@
+.. _examples:
+
+Examples
+========
+
+.. toctree::
+   :maxdepth: 0
+
+   examples/00-quick-start.ipynb
+   examples/01-basic-example.ipynb
+   examples/02-absorbing-object.ipynb
+   examples/03-objects-of-arbitrary-shape.ipynb
+   examples/04-performance-profiling.ipynb
