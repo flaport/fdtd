ae03367fdfa4c9d6b821c1b9024ea101cd86dfd0
BasedOnTechnology <therobotist@gmail.com>
2021-07-10 01:21:48 -0400

    More convenience functions from master

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+
+def pytest_addoption(parser):
+    parser.addoption("--all_backends", action="store_true", help="run all backends")
+
+from fixtures import backend_parametrizer
+
+pytest_generate_tests = backend_parametrizer# perform tests over all backends
