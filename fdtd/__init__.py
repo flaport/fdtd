9be8304aad1dc32edbcf4eee2c60fdbee6c6ed06
flaport <floris.laporte@gmail.com>
2023-05-23 06:12:35 -0700

    Bump version: 0.2.8 → 0.3.0

--- fdtd/__init__.py
+++ fdtd/__init__.py
@@ -1,7 +1,7 @@
 """ Python 3D FDTD Simulator """
 
 __author__ = "Floris laporte"
-__version__ = "0.2.8"
+__version__ = "0.3.0"
 
 from .grid import Grid
 from .sources import PointSource, LineSource, PlaneSource
