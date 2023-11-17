2ed5e974e223110b9315acd36df14f61dfbc255a
flaport <floris.laporte@gmail.com>
2023-11-17 10:15:56 -0800

    repo maintenance

--- fdtd/__init__.py
+++ fdtd/__init__.py
@@ -1,7 +1,7 @@
 """ Python 3D FDTD Simulator """
 
 __author__ = "Floris laporte"
-__version__ = "0.3.0"
+__version__ = "0.3.2"
 
 from .grid import Grid
 from .sources import PointSource, LineSource, PlaneSource
